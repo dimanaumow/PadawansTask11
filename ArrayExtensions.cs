5a5d69aa91b3f0faac62f61138e34fc299fb650f
Dima Naumow <dimanaumow123@gmail.com>
2019-02-26 20:09:14 +0300

    the second attempt

--- ArrayExtensions.cs
+++ ArrayExtensions.cs
@@ -6,8 +6,38 @@ namespace PadawansTask11
     {
         public static int? FindIndex(double[] array, double accuracy)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (accuracy < 0 || accuracy > 1)
+            {
+                throw new ArgumentException();
+            }
+
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            double sumRight = 0;
+
+            double sumLeft = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sumRight += array[i];
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sumRight -= array[i];
+
+                if (Math.Abs(sumRight - sumLeft) < accuracy)
+                {
+                    return i;
+                }
+
+                sumLeft += array[i];
+            }
+
+            return null;
         }
     }
 }
